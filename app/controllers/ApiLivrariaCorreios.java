45929f4f9edb7d9ac46a59dfc4b21bbf4ee07a2e
apla77 <verocidade77@gmail.com>
2019-06-20 01:35:57 -0300

    Criação service Livraria Correios

---
+++ app/controllers/ApiLivrariaCorreios.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+import models.FretePrecoPrazo;
+import play.libs.WS;
+import play.mvc.Controller;
+import play.mvc.Http;
+
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
+import play.libs.XML;
+import play.mvc.Controller;
+
+public class ApiLivrariaCorreios extends Controller{
+	
+	public static void calculoFrete(String cepDestino, String peso, String comprimento, String altura, String largura, double valorDeclarado) {
+		ApiLivrariaCorreios.response.accessControl("*");
+		HttpResponse res = WS.url("http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=08082650&sDsSenha=564321"
+				+ "&sCepOrigem=59910000&sCepDestino="+cepDestino+"&nVlPeso="+peso+"&nCdFormato=1&nVlComprimento="+comprimento+
+				"&nVlAltura="+altura+"&nVlLargura="+largura+"&sCdMaoPropria=n&nVlValorDeclarado="+valorDeclarado
+				+ "&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3").get();
+		
+		String xml = res.getString();
+		String tagV = "<Valor>";
+		String preco = getTagValue(xml, tagV);
+		
+		String tagP = "<PrazoDeEntrega>";
+		String prazo = getTagValue(xml, tagP);
+		
+		FretePrecoPrazo fretePrecoPrazo = new FretePrecoPrazo();
+		fretePrecoPrazo.setPreco(preco);
+		fretePrecoPrazo.setPrazo(prazo);
+		
+		Gson gson = new Gson();
+        
+        String json = gson.toJson(fretePrecoPrazo);
+ 
+        renderText(json);		
+		
+	}
+	
+
+	public static String getTagValue(String xml, String tag) {
+	    String closeTag = new StringBuffer(tag).insert(1, "/").toString();
+	    int from = xml.indexOf(tag) + tag.length();
+	    int to = xml.indexOf(closeTag);
+	
+	    return xml.substring(from, to);
+	}
+
+}
---
+++ app/models/FretePrecoPrazo.java
@@ -0,0 +1,24 @@
+package models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FretePrecoPrazo {
+	
+	public String preco;
+	public String prazo;
+	
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	public String getPrazo() {
+		return prazo;
+	}
+	public void setPrazo(String prazo) {
+		this.prazo = prazo;
+	}
+
+}
